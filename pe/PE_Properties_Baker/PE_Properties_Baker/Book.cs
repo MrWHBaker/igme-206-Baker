29294990f0ad5f0cdbdd87ad8ef7b0b5c68ec7dd
MrWHBaker <whb4352@rit.edu>
2024-10-03 18:53:23 -0400

    Uploaded Properties PE

---
+++ pe/PE_Properties_Baker/PE_Properties_Baker/Book.cs
@@ -0,0 +1,87 @@
+﻿/* Book.cs
+ * Last edited 10-03-24 by Wyatt Baker
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Properties_Baker
+{
+    internal class Book
+    {
+        // -------- Fields ------------------------------------------------------------------------
+        // Get-only
+        private string title;
+        private string author;
+        private int pageCount;
+
+        // Assignable
+        private string owner;
+        private int timesRead;
+
+
+        // -------- Methods -----------------------------------------------------------------------
+        // --- Constructor
+        public Book(string title, string author, int pageCount, string owner)
+        {
+            timesRead = 0;
+            this.title = title;
+            this.author = author;
+            this.pageCount = pageCount;
+            this.owner = owner;
+        }
+
+
+        // --- Properties
+        // Get only
+        public string Title
+        {
+            get { return title; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+        }
+
+        public int PageCount
+        {
+            get { return pageCount; }
+        }
+
+        // Get and Set
+        public string Owner
+        {
+            get { return owner; }
+            set { owner = value; }
+        }
+
+        public int TimesRead
+        {
+            get { return timesRead; }
+            set 
+            {
+                // Only allow timesRead to be updated if the given value is an increase
+                if (timesRead < value)
+                {
+                    timesRead = value;
+                }
+            }
+        }
+
+
+        // --- Behavior
+        public void Print()
+        {
+            Console.WriteLine
+                (
+                $"{title} by {author} has {pageCount} pages. " +
+                $"It is owned by {owner} and has been read {timesRead} times."
+                );
+        }
+
+    } // End Book Class
+}
---
+++ pe/PE_Properties_Baker/PE_Properties_Baker/Program.cs
@@ -0,0 +1,148 @@
+﻿/* 
+ * Wyatt H. Baker
+ * PE - Properties
+ * IGME 206 1
+ *      Instruction document:
+ *      https://docs.google.com/document/d/1z9HVMUdQWjPEswC2UwoDFwaoHdpaLT1Yvo7KtKBBaYs/edit
+ *      
+ * Last edited 10-03-24 by Wyatt H. Baker
+ * Using helper method by Prof. Mesh
+ * No known issues.
+ */
+
+
+namespace PE_Properties_Baker
+{
+    internal class Program
+    {
+        // Input helper written by Prof. Mesh
+        public static string GetPromptedInput(string prompt)
+        {
+            // Always print in white
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Print the prompt
+            Console.Write(prompt + " ");
+
+            // Switch color and get user input (trim too)
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string response = Console.ReadLine().Trim();
+
+            // Switch back to white and then return response.
+            Console.ForegroundColor = ConsoleColor.White;
+            return response;
+        }
+
+        // Method to ensure good user int input
+        // In this case, we'll also ensure that it's a positive number
+        public static int GetValidInt(string prompt)
+        {
+            // Use console color scheme consistent with Professor Mesh's GetPromptedInput
+            Console.ForegroundColor= ConsoleColor.White;
+
+            // Print the prompt
+            Console.Write(prompt + " ");
+
+            // Loop to check that user input is valid
+            bool inputIsValid = false;
+            int input;
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                inputIsValid = int.TryParse(Console.ReadLine(), out input);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                // Re-prompt user on failure
+                if (!inputIsValid || input < 1) 
+                {
+                    Console.WriteLine("Please enter a properly formatted positive integer.");
+                    Console.Write(prompt + " ");
+                }
+            } while (!inputIsValid || input < 1);
+
+            return input;
+        }
+
+        // Main program method
+        static void Main(string[] args)
+        {
+            // --- Declare Variables --------------------------------------------------------------
+            string userInput;
+
+            // --- Introduce program and Create Book object via user input ------------------------
+            Console.WriteLine("Welcome to Book Simulator 2020\n");
+
+            Book newBook = new Book
+                (
+                GetPromptedInput("What is the book's title?"),
+                GetPromptedInput("Who is the book's author?"),
+                GetValidInt("How many pages does it have?"),
+                GetPromptedInput("Who is the book's current owner?")
+                );
+
+            // --- Main program loop --------------------------------------------------------------
+            do
+            {
+                // Prompt user for the next action
+                userInput = GetPromptedInput("\nWhat would you like to do?").ToLower();
+
+                switch (userInput)
+                {
+                    // Get information
+                    case "title":
+                        Console.WriteLine($"The title is {newBook.Title}.");
+                        break;
+
+                    case "author":
+                        Console.WriteLine($"The author is {newBook.Author}.");
+                        break;
+
+                    case "pages":
+                        Console.WriteLine($"The book has {newBook.PageCount} pages.");
+                        break;
+
+                    // Allow user to get or set the owner
+                    case "owner":
+                        userInput = GetPromptedInput
+                            ("Would you like to change the owner (yes/no)?");
+
+                        if (userInput == "yes")
+                        {
+                            userInput = GetPromptedInput
+                                ("Who is the new owner?");
+                            newBook.Owner = userInput;
+
+                            Console.WriteLine($"The new owner is {newBook.Owner}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Ok. {newBook.Owner} is still the owner.");
+                        }
+                        break;
+
+                    // Update the times read
+                    case "read":
+                        newBook.TimesRead += 1;
+                        Console.WriteLine($"The total times read is now {newBook.TimesRead}.");
+                        break;
+
+                    // Print all info
+                    case "print":
+                        newBook.Print();
+                        break;
+
+                    // Exit program
+                    case "done":
+                        Console.WriteLine("Goodbye.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid input, please try again.");
+                        break;
+                }
+
+            } while (true);
+        }
+    }
+}
