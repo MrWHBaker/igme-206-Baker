2431534158ce1f8f2b420e6a2fb299ab09b47ab3
MrWHBaker <whb4352@rit.edu>
2024-10-12 23:59:36 -0400

    Uploaded Abstraction PE

---
+++ pe/Abstraction_Polymorphism_Demo/Pets/Goldfish.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction_Polymorphism_Demo.Pets
+{
+    /// <summary>
+    /// Goldfish are pets that say NOTHING
+    /// </summary>
+    internal class Goldfish : Pet
+    {
+        // Parameterized Goldfish constructor 
+        public Goldfish(string name, DateTime birthday)
+            : base(name, birthday, "goldfish")
+        {
+        }
+
+        // We also want to override Speak so that this Goldfish can't talk!
+        public override void Speak()
+        {
+            Console.WriteLine(this.Name + " says NOTHING to the likes of YOU.");
+        }
+
+    }
+}
