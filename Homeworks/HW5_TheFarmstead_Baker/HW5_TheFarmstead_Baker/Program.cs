3ef8a940762694cf57c91f5c49698e64a45e9487
MrWHBaker <whb4352@rit.edu>
2024-10-14 11:27:45 -0400

    Started HW5 - The Farmstead

---
+++ Homeworks/HW5_TheFarmstead_Baker/HW5_TheFarmstead_Baker/Program.cs
@@ -0,0 +1,22 @@
+﻿/* 
+ * Wyatt H. Baker
+ * HW 5 - The Farmstead
+ * IGME 206 1
+ *      Instruction document:
+ *      https://docs.google.com/document/d/1xnF9pZIhLC-PW3OAOktW15VzMtAktTZWnHEsuQSDBpQ/edit?tab=t.0
+ *      
+ * Last edited 10-14-24 by Wyatt H. Baker
+ * No known issues.
+ */
+
+
+namespace HW5_TheFarmstead_Baker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
