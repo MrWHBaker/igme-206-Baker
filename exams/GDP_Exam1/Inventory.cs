70ab1f269f851538bca897a234502daa7c05f82d
MrWHBaker <whb4352@rit.edu>
2024-10-17 15:36:23 -0400

    Ensuring that there's no issue with Github

---
+++ exams/GDP_Exam1/Inventory.cs
@@ -0,0 +1,86 @@
+﻿//****************************************************************
+// DO NOT modify anything in this file *EXCEPT* where marked
+// explicitly with TODO comments!
+//****************************************************************
+namespace GDP_Exam_1
+{
+    /// <summary>
+    /// A standalone class to hold Item object instances
+    /// </summary>
+    class Inventory
+    {
+        // NO additional fields are permitted.
+        private List<Item> items = new List<Item>();
+
+        /// <summary>
+        /// Return the number of items within the
+        /// inventory's list. Nothing can be changed.
+        /// </summary>
+        public int NumberItems
+        {
+            // TODO: Complete the property
+            get 
+            {
+                return 0; // REPLACE
+            }
+        }
+
+        /// <summary>
+        /// TODO: Complete the AddItem method to add a provided Item reference
+        /// into the inventory list
+        /// </summary>
+        public void AddItem(Item itemToAdd)
+        {
+        }
+
+        /// <summary>
+        /// TODO: Complete the PrintSummary method to print the number of items
+        /// in the inventory and then a summary of each item.
+        /// </summary>
+        public void PrintSummary()
+        {
+        }
+
+
+        /// <summary>
+        /// TODO: Complete the CalculateTotalWeight method to return the total
+        /// weight of all items in the inventory
+        /// </summary>
+        private double CalculateTotalWeight()
+        {
+            double total = 0;
+            return total;
+        }
+
+        /// <summary>
+        /// TODO: Complete CalculateTotalWeight method to return the total
+        /// damage of all weapons in the inventory
+        /// </summary>
+        private double CalculateTotalDamage()
+        {
+            double total = 0;
+            return total;
+        }
+
+        /// <summary>
+        /// Loads items from a file line by line
+        /// </summary>
+        public void LoadItems(string filename)
+        {
+            StreamReader input = null;
+
+            // TODO: Add exception handling
+                input = new StreamReader(filename);
+                string line = null;
+                while((line = input.ReadLine()) != null)
+                {
+                    // TODO: For each line, seperate the data and create
+                    // new Food or Weapon objects appropriately
+                }
+            if (input != null)
+            {
+                input.Close();
+            }
+        }
+    }
+}
---
+++ exams/GDP_Exam1/Weapon.cs
@@ -0,0 +1,38 @@
+﻿//****************************************************************
+// DO NOT modify anything in this file *EXCEPT* where marked
+// explicitly with TODO comments!
+//****************************************************************
+namespace GDP_Exam_1
+{
+    /// <summary>
+    /// Inherits from item and adds data & behavior specific for weapons
+    /// </summary>
+    // TODO: Make this inherit from item
+    class Weapon
+    {
+        // NO additional fields or properties are permitted.
+        private double weight;
+        private int damage;
+
+        /// <summary>
+        /// Return how much damage this weapon can do
+        /// </summary>
+        public int Damage { get { return damage; } }
+
+        /// <summary>
+        /// TODO: Add the Item class's required abstract Weight
+        /// property.
+        /// </summary>
+
+        /// <summary>
+        /// TODO: Add a parameterized constructor using the constructor
+        /// calls in Main & the writeup as a guide for what this constructor
+        /// must do. Leverage the base class constructor as needed.
+        /// </summary>
+
+        /// <summary>
+        /// TODO: Override ToString to leverage the base class ToString 
+        /// and add on the amount of damage this weapon does.
+        /// </summary>
+    }
+}
