8b942c779f6faa848c43cdcbf1fb2ffbe557c333
MrWHBaker <whb4352@rit.edu>
2024-11-16 13:54:19 -0500

    Submitted UPE-07

--- upe/projects/UPE_07_Forces/Assets/Scripts/PhysicsObject.cs
+++ upe/projects/UPE_07_Forces/Assets/Scripts/PhysicsObject.cs
@@ -22,7 +22,7 @@ public class PhysicsObject : MonoBehaviour
 
     [SerializeField]
     private bool hasGravity;
-    private float gravityStrength;
+    private float gravityStrength = 1f;
 
     private float maxSpeed;
 
@@ -33,15 +33,43 @@ public class PhysicsObject : MonoBehaviour
     // ======== Force application methods ==========================================================
 
     // Method to apply the force of friction to an object
+    private void ApplyFriction()
+    {
+        // Friction applies the opposite of velocity scaled to a friction coefficient
+        Vector3 friction = velocity * -1;
+        friction.Normalize();
+        friction = friction * frictionCoeff;
+        acceleration += friction/mass;
+    }
 
 
     // Method to apply the force of gravty to an object
-
+    private void ApplyGravity()
+    {
+        // Gravity does not take mass into account
+        acceleration.y -= gravityStrength;
+    }
 
     // OPTIONAL apply rotation
     
 
     // Method to bounce off window edges
+    private void Bounce()
+    {
+        float bounce = -0.9f;
+
+        if (Mathf.Abs(position.y) > 5)
+        {
+            position.y = 5 * Mathf.Sign(position.y);
+            velocity.y *= bounce;
+        }
+
+        if (Mathf.Abs(position.x) > 8 )
+        {
+            position.x = 8 * Mathf.Sign(position.x);
+            velocity.x *= bounce;
+        }    
+    }
 
 
     // ======== Standard behavior ================================================================
@@ -49,11 +77,34 @@ public class PhysicsObject : MonoBehaviour
     void Start()
     {
         mouseTracker = GetComponent<MouseTracker>();
+        position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        // Use applied forces to update position and direction
+        if (hasGravity) { ApplyGravity(); }
+        if (hasFriction) { ApplyFriction(); }
+
+        // Apply a force toward the cursor
+        acceleration += mouseTracker.MousePuller(position)/mass;
+
+        velocity += acceleration * Time.deltaTime;
+
+        // Bounce!
+        Bounce();
+
+        position += velocity * Time.deltaTime;
+
+        direction = velocity.normalized;
+
+        position.z = 0f;
+        transform.position = position;
+
+        // Reset accelleration
+        acceleration = Vector3.zero;
+
     }
 }
